db9cd3eba1a768ff4a7c9708a86cb31ae0663365
savan patel <savankansagara1005@gmail.com>
2025-06-13 15:25:27 +0530

    Fix case-sensitive import for NavBar

--- FRONTEND/src/RootLayout.jsx
+++ FRONTEND/src/RootLayout.jsx
@@ -3,13 +3,13 @@ import HomePage from './pages/HomePage.jsx'
 import LoginForm from './components/LoginForm.jsx'
 import AuthPage from './pages/AuthPage.jsx'
 import { Outlet } from '@tanstack/react-router'
-import NavBar from './components/Navbar.jsx'
+import NavBar from './components/NavBar.jsx'
 
 
 const RootLayout = () => {
   return (
    <>
-   <NavBar />
+   <NavBar/>
    <Outlet />
    </>
   )
