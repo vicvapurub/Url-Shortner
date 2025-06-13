251eed4f315711d5093e5926ed80de4b8649271a
Savan Kansagara <savankansagara1005@gmail.com>
2025-06-13 17:37:01 +0530

    Update axiosinstance.js

--- FRONTEND/src/utils/axiosinstance.js
+++ FRONTEND/src/utils/axiosinstance.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  timeout: 10000, // 10 seconds timeout
+  timeout: 20000, // 10 seconds timeout
   withCredentials: true
 });
 
